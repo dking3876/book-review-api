5c639a19d2771ca54deffcc017fd06c170986375
dking3876 <dking3876@msn.com>
2018-05-25 14:56:34 -0400

    first commit

---
+++ app/Http/Resources/BookCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BookCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'pagination' => [
+                'total' => $this->total(),
+                'count' => $this->count(),
+                'per_page' => $this->perPage(),
+                'current_page' => $this->currentPage(),
+                'total_pages' => $this->lastPage()
+            ]
+            ];
+    }
+    public function with($request){
+        return [
+            'meta'  => [
+                'somethincool'  => 'value',
+                'something else'    => 'yippie'
+            ]
+            ];
+    }
+}
